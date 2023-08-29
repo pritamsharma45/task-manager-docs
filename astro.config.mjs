3f9aafae37b0606cca32b21403bb60354a8a7541
pritamsharma45 <notifications.borl@gmail.com>
2023-08-29 19:28:47 +0530

    sdfas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
